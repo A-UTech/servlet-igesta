b9da32aed954dc3f1d971d7edaff1c930fa64db3
Lima152010 <lucaslbr1a2b@gmail.com>
2025-10-17 10:52:30 -0300

    refactor: Arrumando o servlet de Perfil

--- src/main/java/com/backigesta/servlet/Perfil.java
+++ src/main/java/com/backigesta/servlet/Perfil.java
@@ -1,6 +1,7 @@
 package com.backigesta.servlet;
 
 import com.backigesta.dao.AdminDAO;
+import com.backigesta.dao.DAO;
 import com.backigesta.dao.EmpresasDAO;
 import com.backigesta.dao.FuncionariosDAO;
 import com.backigesta.model.Admin;
@@ -8,7 +9,6 @@ import com.backigesta.model.Empresas;
 import com.backigesta.model.Funcionarios;
 import com.backigesta.model.Usuarios;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,11 +17,50 @@ import jakarta.servlet.http.Part;
 
 import java.io.IOException;
 
-@WebServlet("/uploadFoto")
-@MultipartConfig
-public class UploadFoto extends HttpServlet {
+@WebServlet(urlPatterns = {"/atualizarPerfil","/uploadFoto"})
+public class Perfil extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String caminho = request.getServletPath();
+        if (caminho.equals("/atualizarPerfil")) {
+            atualizarPerfil(request,response);
+        } else if (caminho.equals("/uploadFoto")) {
+            uploadFoto(request,response);
+        }
+    }
+
+    protected void atualizarPerfil(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        String nome = request.getParameter("name");
+        String email = request.getParameter("email");
+        String senha = request.getParameter("password");
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        String tipo = request.getParameter("tipo");
+
+        DAO dao = null;
+
+        switch (tipo) {
+            case "Admin": {
+                dao = new AdminDAO();
+            }
+            break;
+            case "Empresas": {
+                dao = new EmpresasDAO();
+            }
+            default: {
+                //Colocar pagina de erro
+            }
+            break;
+        }
+        Usuarios user = dao.selecionarPorId(id);
+        user.setNome(nome);
+        user.setEmail(email);
+        user.setSenha(senha);
+
+        if(dao.atualizar(user)){}
+    }
+
+    protected void uploadFoto(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //Coletando os parâmetros para encontrar o Usuario especificado.
         int id = Integer.parseInt(request.getParameter("id"));
         String tipo = request.getParameter("tipo");
